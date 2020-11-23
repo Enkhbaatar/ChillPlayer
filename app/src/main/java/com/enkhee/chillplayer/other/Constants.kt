c00e03ae45ddae0d951182fb1fbbd6f49184572a
Enkhbaatar <enkhbaatar.tum@gmail.com>
2020-11-22 21:27:36 -0600

    * Create SongViewModel.kt * Added currentPlaybackPosition calculate extension method for PlayBackStateCompat

--- app/src/main/java/com/enkhee/chillplayer/other/Constants.kt
+++ app/src/main/java/com/enkhee/chillplayer/other/Constants.kt
@@ -2,9 +2,12 @@ package com.enkhee.chillplayer.other
 
 object Constants {
     const val SONG_COLLECTION = "songs"
+
+    /** Notification constants*/
     const val NOTIFICATION_ID = 10000
     const val NOTIFICATION_CHANEL_ID = "music"
 
+    /** Song list index constants*/
     const val FIRST_SONG_INDEX = 0
     const val SONG_START_POSITION = 0L
     const val INVALID_INDEX = -1
@@ -12,6 +15,7 @@ object Constants {
     const val MEDIA_ROOT_ID = "root_id"
 
     const val NETWORK_ERROR = "NETWORK_ERROR"
+    const val UPDATE_PLAYER_POSITION_INTERVAL = 100L
 
     /** Default error message */
     const val UNKNOWN_ERROR_OCCURRED = "An unknown error occurred"
